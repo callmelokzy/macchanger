8f860563c1f0d8b198d9fb0676ef3c75f2a3a115
Vishnu JS <chirakkal.vishnu7@gmail.com>
2022-09-11 18:27:04 -0400

    Add files via upload

---
+++ macchanger.py
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/python3
+import optparse
+import subprocess
+
+
+def arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+    return parser.parse_args()
+
+
+def macx(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+(options, arguments) = arguments()
+macx(options.interface, options.new_mac)
